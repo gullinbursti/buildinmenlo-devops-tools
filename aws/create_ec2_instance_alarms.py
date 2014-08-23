8a76fb6fb93088295857304963a055003a99a8d1
Pedro <digitalrounin@gmail.com>
2014-08-22 20:35:18 -0700

    Add config files and other nice things

--- aws/create_ec2_instance_alarms.py
+++ aws/create_ec2_instance_alarms.py
@@ -3,9 +3,49 @@
 import boto.ec2.cloudwatch
 import sys
 import os
+import logging
+import ConfigParser
+import argparse
+
+AWS_REGION = None
+AWS_SNS_TOPIC = None
+AWS_ACCESS_KEY_ID = None
+AWS_SECRET_ACCESS_KEY = None
+
+CONFIG_SECTION = 'aws'
+CONFIG_FILE = os.path.join(
+    os.environ['HOME'], '.builtinmenlo', 'devops-tools.cnf')
+LOG_LEVEL = logging.INFO
 
 AWS_REGION = os.environ.get("AWS_EC2_REGION", "us-east-1")
 TOPIC = 'arn:aws:sns:us-east-1:892810128873:00-ops-selfieclub-monitoring'
+
+
+def main():
+    logging.basicConfig(level=LOG_LEVEL)
+    read_configuration()
+    args = process_args()
+    process_instances(args.instance_ids)
+
+
+def process_instances(instance_ids):
+    cloudwatch_conn = boto.ec2.cloudwatch.connect_to_region(
+        AWS_REGION,
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    for instance_id in instance_ids:
+        process_instance(cloudwatch_conn, instance_id)
+    cloudwatch_conn.close()
+
+
+def process_instance(cloudwatch_conn, instance_id):
+    instance_info = get_instance_info(instance_id)
+    cloudwatch_conn = boto.ec2.cloudwatch.connect_to_region(AWS_REGION)
+    create_status_alarm(cloudwatch_conn, instance_info)
+    create_cpu_alarm(cloudwatch_conn, instance_info)
+    create_swap_alarm(cloudwatch_conn, instance_info)
+    create_memory_alarm(cloudwatch_conn, instance_info)
+    create_disk_alarm(cloudwatch_conn, instance_info)
 
 
 def alarm_name_prefix(instance_name):
@@ -148,17 +188,35 @@ def create_disk_alarm(cloudwatch_conn, instance_info):
     cloudwatch_conn.put_metric_alarm(alarm)
 
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print "Usage: create_status_alarm.py <instanceid>"
-        sys.exit(2)
-    instance_id = sys.argv[1]
-    instance_info = get_instance_info(instance_id)
+def read_configuration():
+    logging.debug("Reading configuration: %s[%s]", CONFIG_FILE, CONFIG_SECTION)
 
-    cloudwatch_conn = boto.ec2.cloudwatch.connect_to_region(AWS_REGION)
-    create_status_alarm(cloudwatch_conn, instance_info)
-    create_cpu_alarm(cloudwatch_conn, instance_info)
-    create_swap_alarm(cloudwatch_conn, instance_info)
-    create_memory_alarm(cloudwatch_conn, instance_info)
-    create_disk_alarm(cloudwatch_conn, instance_info)
-    cloudwatch_conn.close()
+    config = ConfigParser.ConfigParser()
+    config.read(CONFIG_FILE)
+
+    global AWS_REGION, AWS_SNS_TOPIC, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+    AWS_REGION = config.get(CONFIG_SECTION, 'aws_region')
+    AWS_SNS_TOPIC = config.get(CONFIG_SECTION, 'aws_sns_topic')
+    AWS_ACCESS_KEY_ID = config.get(CONFIG_SECTION, 'aws_access_key_id')
+    AWS_SECRET_ACCESS_KEY = config.get(CONFIG_SECTION, 'aws_secret_access_key')
+
+    logging.info("Configuration: %s", {
+        'aws_region': AWS_REGION,
+        'aws_sns_topic': AWS_SNS_TOPIC,
+        'aws_access_key_id': 'XXXXXXXX',
+        'aws_secret_access_key': 'YYYYYYYY'})
+
+
+def process_args():
+    parser = argparse.ArgumentParser(
+        description='Setup AWS ClousWatch alarms for EC2 instances.')
+    parser.add_argument(
+        'instance_ids',
+        metavar='INSTANCE_ID',
+        nargs='+',
+        help='EC2 insance identifier(s)')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
