30d578a30825021dba6da95763d608ac4dbfd44f
Pedro H <pedro@builtinmenlo.com>
2014-08-26 01:02:57 +0000

    Simple script to generate URLs from club creation

---
+++ selfieclub-admin/club-creator_log_to_url.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+import re
+import sys
+
+with open(str(sys.argv[1]), 'r') as input:
+    for line in input:
+        line = line.strip()
+        matches = re.search('INFO:root:Created club:  (.*)', line)
+        if not matches:
+            continue
+        exec("data = {}".format(matches.groups()[0]))
+        print("http://joinselfie.club/{}/{}".format(data['owner_name'], data['club_name']))
