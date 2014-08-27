43d02a0d623a3c36c3ba3730a7a84cae6eb58e7e
Pedro <digitalrounin@gmail.com>
2014-08-27 15:44:12 -0700

    Minor

--- selfieclub-admin/delete-user.sql
+++ selfieclub-admin/delete-user.sql
@@ -1,8 +1,8 @@
-DELETE FROM `tblChallengeVotes` WHERE `user_id` = 152805;
-DELETE FROM `tblChallenges` WHERE `creator_id` = 152805;
-DELETE FROM `tblUserPhones` WHERE `user_id` = 152805;
-DELETE FROM `club_member` WHERE `user_id` = 152805;
-DELETE FROM `tblFlaggedUserApprovals` WHERE `user_id` = 152805;
-DELETE from `club_member` WHERE `club_id` IN (SELECT `id` FROM `club` WHERE `owner_id` = 152805);
-DELETE FROM `club` WHERE `owner_id` = 152805;
-DELETE FROM `tblUsers` WHERE `id` = 152805;
+DELETE FROM `tblChallengeVotes` WHERE `user_id` = 195027;
+DELETE FROM `tblChallenges` WHERE `creator_id` = 195027;
+DELETE FROM `tblUserPhones` WHERE `user_id` = 195027;
+DELETE FROM `club_member` WHERE `user_id` = 195027;
+DELETE FROM `tblFlaggedUserApprovals` WHERE `user_id` = 195027;
+DELETE from `club_member` WHERE `club_id` IN (SELECT `id` FROM `club` WHERE `owner_id` = 195027);
+DELETE FROM `club` WHERE `owner_id` = 195027;
+DELETE FROM `tblUsers` WHERE `id` = 195027;
