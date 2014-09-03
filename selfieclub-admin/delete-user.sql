ff6801d57d26deec609adcf7cdce1b75b9dfa5b4
Pedro <digitalrounin@gmail.com>
2014-09-03 16:02:58 -0700

    Improved SQL

--- selfieclub-admin/delete-user.sql
+++ selfieclub-admin/delete-user.sql
@@ -1,8 +1,8 @@
-DELETE FROM `tblChallengeVotes` WHERE `user_id` = 195027;
-DELETE FROM `tblChallenges` WHERE `creator_id` = 195027;
-DELETE FROM `tblUserPhones` WHERE `user_id` = 195027;
-DELETE FROM `club_member` WHERE `user_id` = 195027;
-DELETE FROM `tblFlaggedUserApprovals` WHERE `user_id` = 195027;
-DELETE from `club_member` WHERE `club_id` IN (SELECT `id` FROM `club` WHERE `owner_id` = 195027);
-DELETE FROM `club` WHERE `owner_id` = 195027;
-DELETE FROM `tblUsers` WHERE `id` = 195027;
+DELETE FROM `tblChallengeVotes` WHERE `user_id` = 195020;
+DELETE FROM `tblChallenges` WHERE `creator_id` = 195020;
+DELETE FROM `tblUserPhones` WHERE `user_id` = 195020;
+DELETE FROM `club_member` WHERE `user_id` = 195020;
+DELETE FROM `tblFlaggedUserApprovals` WHERE `user_id` = 195020;
+DELETE from `club_member` WHERE `club_id` IN (SELECT `id` FROM `club` WHERE `owner_id` = 195020);
+DELETE FROM `club` WHERE `owner_id` = 195020;
+DELETE FROM `tblUsers` WHERE `id` = 195020;
