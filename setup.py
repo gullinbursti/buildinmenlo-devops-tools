d55bda9518f527c5d2572722563f99051233e4e7
Pedro <digitalrounin@gmail.com>
2014-09-12 11:59:17 -0700

    Forgot to add one of the keen scripts

--- setup.py
+++ setup.py
@@ -13,4 +13,5 @@ setup(
         path.join('aws', 'create_rds_instance_alarms.py'),
         path.join('reporting', 'db-45-day-report.py'),
         path.join('reporting', 'keen-event-collection-action-info.py'),
+        path.join('reporting', 'keen-event-collection-info.py'),
         path.join('selfieclub-admin', 'delete-user.py')])
