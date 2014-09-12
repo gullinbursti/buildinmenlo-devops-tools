6d78d1d31fe2ad73454cc06cdbae971789c5d174
Pedro <digitalrounin@gmail.com>
2014-09-12 11:24:50 -0700

    Handle installation

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ setup(
     author='Pedro H',
     author_email='pedro@builtinmenlo.com',
     scripts=[
+        path.join('aws', 'create_ec2_instance_alarms.py'),
+        path.join('aws', 'create_elasticache_instance_alarms.py'),
+        path.join('aws', 'create_elb_alarms.py'),
+        path.join('aws', 'create_rds_instance_alarms.py'),
         path.join('reporting', 'db-45-day-report.py'),
         path.join('reporting', 'keen-event-collection-action-info.py'),
-        path.join('reporting', 'keen-event-collection-info.py')])
+        path.join('selfieclub-admin', 'delete-user.py')])
