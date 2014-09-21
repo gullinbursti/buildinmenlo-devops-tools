8e63a7bc2793231faa9859caf98aee30814b3507
Pedro <digitalrounin@gmail.com>
2014-09-21 14:49:13 +0700

    Hacked script for backing up Elasticsearch

---
+++ elasticsearch/README.md
@@ -0,0 +1,4 @@
+
+This is a quick hack based off of the scripts in `buildinmenlo-devops-tools/old-scripts/elasticsearch/`.  Read the
+files in `buildinmenlo-devops-tools/old-scripts/elasticsearch` for more information.
+
---
+++ elasticsearch/backup.sh
@@ -0,0 +1,45 @@
+# SCRIPTS=/home/volley/admin/bin/elasticsearch
+TO_FOLDER=.
+FROM=/elasticsearch/data
+
+DATE=`date +%Y-%m-%d_%H`
+TO=$TO_FOLDER/$DATE
+echo "rsync from $FROM to $TO"
+# the first times rsync can take a bit long - do not disable flusing
+rsync -a $FROM $TO
+
+# now disable flushing and do one manual flushing
+#----
+# $SCRIPTS/es-flush-disable.sh true
+curl -XPUT 'localhost:9200/_settings' -d '{
+   "index" : {
+      "translog.disable_flush" : "'true'"
+   }
+}'
+#----
+
+
+
+#----
+# $SCRIPTS/es-flush.sh
+curl -XPOST 'localhost:9200/_flush'
+#----
+
+
+# ... and sync again
+rsync -a $FROM $TO
+
+#----
+# $SCRIPTS/es-flush-disable.sh false
+curl -XPUT 'localhost:9200/_settings' -d '{
+   "index" : {
+      "translog.disable_flush" : "'false'"
+   }
+}'
+#----
+
+# now zip up final product
+echo "\nzipping $TO $TO.tar.gz "
+cd $TO_FOLDER
+/bin/tar zcf $DATE.tar.gz $DATE
+/bin/rm -rf $DATE
