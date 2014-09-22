b0feba6f19231862e0d25595a509c620d6559a92
Pedro <digitalrounin@gmail.com>
2014-09-22 14:54:45 +0700

    Moved things around to make it 'cleaner'?

--- elasticsearch/README.md
+++
@@ -1,4 +0,0 @@
-
-This is a quick hack based off of the scripts in `buildinmenlo-devops-tools/old-scripts/elasticsearch/`.  Read the
-files in `buildinmenlo-devops-tools/old-scripts/elasticsearch` for more information.
-
--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@ from distutils.core import setup
 
 setup(
     name='buildinmenlo-devops-tools',
-    version='0.1.0',
+    version='0.2.0',
     author='Pedro H',
     author_email='pedro@builtinmenlo.com',
     scripts=[
-        path.join('aws', 'create_ec2_instance_alarms.py'),
-        path.join('aws', 'create_elasticache_instance_alarms.py'),
-        path.join('aws', 'create_elb_alarms.py'),
-        path.join('aws', 'create_rds_instance_alarms.py'),
-        path.join('reporting', 'db-45-day-report.py'),
-        path.join('reporting', 'keen-event-collection-action-info.py'),
-        path.join('reporting', 'keen-event-collection-info.py'),
-        path.join('selfieclub-admin', 'delete-user.py')])
+        path.join('scripts', 'aws', 'create_ec2_instance_alarms.py'),
+        path.join('scripts', 'aws', 'create_elasticache_instance_alarms.py'),
+        path.join('scripts', 'aws', 'create_elb_alarms.py'),
+        path.join('scripts', 'aws', 'create_rds_instance_alarms.py'),
+        path.join('scripts', 'reporting', 'db-45-day-report.py'),
+        path.join('scripts', 'reporting',
+                  'keen-event-collection-action-info.py'),
+        path.join('scripts', 'reporting', 'keen-event-collection-info.py'),
+        path.join('scripts', 'selfieclub-admin', 'delete-user.py')])
