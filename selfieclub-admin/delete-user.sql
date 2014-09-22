b13a5d39e4b6415d95c7aa6e1f0ed219550e897d
Pedro H <pedro@builtinmenlo>
2014-09-22 03:56:29 +0000

    Added emo tables

--- selfieclub-admin/delete-user.sql
+++ selfieclub-admin/delete-user.sql
@@ -1,8 +1,12 @@
-DELETE FROM `tblChallengeVotes` WHERE `user_id` = 195020;
-DELETE FROM `tblChallenges` WHERE `creator_id` = 195020;
-DELETE FROM `tblUserPhones` WHERE `user_id` = 195020;
-DELETE FROM `club_member` WHERE `user_id` = 195020;
-DELETE FROM `tblFlaggedUserApprovals` WHERE `user_id` = 195020;
-DELETE from `club_member` WHERE `club_id` IN (SELECT `id` FROM `club` WHERE `owner_id` = 195020);
-DELETE FROM `club` WHERE `owner_id` = 195020;
-DELETE FROM `tblUsers` WHERE `id` = 195020;
+DELETE FROM `tblChallengeVotes` WHERE `user_id` = 192045;
+DELETE FROM `tbl_status_update_emotion` WHERE `status_update_id` IN (SELECT `id` FROM `tblChallenges` WHERE `creator_id` =192045);
+DELETE FROM `tblChallenges` WHERE `creator_id` = 192045;
+DELETE FROM `tblUserPhones` WHERE `user_id` = 192045;
+DELETE FROM `club_member` WHERE `user_id` = 192045;
+DELETE FROM `tblFlaggedUserApprovals` WHERE `user_id` = 192045;
+DELETE from `club_member` WHERE `club_id` IN (SELECT `id` FROM `club` WHERE `owner_id` = 192045);
+DELETE FROM `club` WHERE `owner_id` = 192045;
+DELETE FROM `moji_invite` WHERE `member_id` = 192045;
+DELETE FROM `tblUsers` WHERE `id` = 192045;
+
+
