1c3c17444f07bebeb5bf083c1fdfc5f2f746e4a2
Pedro <digitalrounin@gmail.com>
2014-09-25 11:03:05 +0700

    Corrected pylint issues

--- scripts/selfieclub-admin/club-creator_log_to_url.py
+++ scripts/selfieclub-admin/club-creator_log_to_url.py
@@ -1,13 +1,16 @@
 #! /usr/bin/env python
+# pylint: disable=invalid-name, exec-used
 
 import re
 import sys
 
-with open(str(sys.argv[1]), 'r') as input:
-    for line in input:
+with open(str(sys.argv[1]), 'r') as input_file:
+    for line in input_file:
         line = line.strip()
         matches = re.search('INFO:root:Created club:  (.*)', line)
         if not matches:
             continue
+        data = {}
         exec("data = {}".format(matches.groups()[0]))
-        print("http://joinselfie.club/{}/{}".format(data['owner_name'], data['club_name']))
+        print("http://joinselfie.club/{}/{}".format(data['owner_name'],
+                                                    data['club_name']))
